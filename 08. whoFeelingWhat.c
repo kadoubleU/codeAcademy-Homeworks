27a4aef6031d01c81a4e90587e8e6e1315297920
kadoubleU <108259380+kadoubleU@users.noreply.github.com>
2022-06-27 10:39:27 +0300

    Create 08. whoFeelingWhat.c

---
+++ 08. whoFeelingWhat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+    
+int main()
+{
+    /*Задайте стойности на променливите:
+    char feeling[25] ;
+    char who[25] ;
+    char what [25] ;
+    И отпечатайте изречение на екран в последователност „Who feeling what“
+    printf("%s %s %s!", who, feeling,what);*/
+    
+    char feeling[5] = "love ";
+    char who[2] = "I ";
+    char what[12] = "programming!";
+    
+    printf("%s", who);
+    
+    return 0;
+    
+}
