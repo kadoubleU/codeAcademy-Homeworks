0fda33b54cd48642bd18141c1f8f54d6ded18b90
kadoubleU <108259380+kadoubleU@users.noreply.github.com>
2022-06-27 12:38:10 +0300

    Create 12. monthDays.c

---
+++ 12. monthDays.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main() {
+	
+	char month;
+	
+	printf("Write month number: ");
+	scanf("%d", &month);
+	
+	switch (month) {
+		case 1:
+			printf("January has 31 days.\n");
+			break;
+		case 2:
+			printf("February has 28 days.");
+			break;
+		case 3:
+			printf("March has 31 days.");
+			break;
+		case 4:
+			printf("April has 30 days.");
+			break;
+		case 5:
+			printf("May has 31 days.");
+			break;
+		case 6:
+			printf("June has 30 days.");
+			break;
+		case 7:
+			printf("July has 31 days.");
+			break;
+		case 8:
+			printf("August has 31 days.");
+			break;
+		case 9:
+			printf("September has 30 days.");
+			break;
+		case 10:
+			printf("October has 31 days.");
+			break;
+		case 11:
+			printf("November has 30 days.");
+			break;
+		case 12:
+			printf("December has 31 days.");
+			break;
+		default:
+			printf("Wrong Choice.");
+			break;
+			
+	}
+	return 0;
+	
+}
